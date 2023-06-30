6e864c31adb8bce9ab2bb6ebc72189282e9a9004
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-06-30 15:06:34 -0500

    owners api endpoint works
    - getAllOwners api endpoint returns data to the browser

---
+++ DoggyDetails/DoggyDetails/Controllers/OwnersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using DoggyDetails.Models;
+using Newtonsoft.Json;
+
+namespace DoggyDetails.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        public OwnersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetAllOwners")]
+        public string GetOwners()
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DoggyDetailsConnectionString").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("select * from Owner", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            List<Owner> ownerList = new List<Owner>();
+            Response response = new Response();
+            if(dt.Rows.Count > 0)
+            {
+                for(int i = 0; i < dt.Rows.Count; i++)
+                {
+                    Owner owner = new Owner();
+                    owner.OwnerID = Convert.ToInt32(dt.Rows[i]["OwnerID"]);
+                    owner.FirstName = Convert.ToString(dt.Rows[i]["FirstName"]);
+                    owner.LastName = Convert.ToString(dt.Rows[i]["LastName"]);
+                    owner.AccountEmail = Convert.ToString(dt.Rows[i]["AccountEmail"]);
+                    owner.AccountPassword = Convert.ToString(dt.Rows[i]["AccountPassword"]);
+                    ownerList.Add(owner);
+                }
+            }
+            if(ownerList.Count > 0)
+            {
+                return JsonConvert.SerializeObject(ownerList);
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.ErrorMessage = "No Data Found";
+                return JsonConvert.SerializeObject(response);
+            }
+        }
+    }
+}
---
+++ DoggyDetails/DoggyDetails/Models/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace DoggyDetails.Models
+{
+    public class Owner
+    {
+        public int OwnerID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? AccountEmail { get; set; }
+        public string? AccountPassword { get; set; }
+    }
+}
