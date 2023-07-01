50d5f56159fb4a7859de7f7813cab82a334c95cf
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-06-30 20:36:41 -0500

    navbar wip
    navbar work in progress (power is flickering)

--- DoggyDetails.ui/javascripts/main.js
+++ DoggyDetails.ui/javascripts/main.js
@@ -1,29 +1,22 @@
-console.log("If you see this your javascript file is connected.");
 
-
-
-
-const addEventListeners = () => {
-   getAllOwnersButton = document.getElementById("getAllOwners");
-
-   getAllOwnersButton.addEventListener("click", getAllOwners);
-}
-
- const getAllOwners = async () => {
+const getAllOwners = async () => 
+{
     console.log("you clicked the button");
     const url = "https://localhost:7260/api/Owners/GetAllOwners";
-    //let promise = fetch(url);
-
     let response = await fetch(url);
-
     let ownersAsJson = await response.json();
-    
     console.log(ownersAsJson);
 }
 
-const init = () => {
+const addEventListeners = () => 
+{
+    getAllOwnersButton = document.getElementById("getAllOwners");
+    getAllOwnersButton.addEventListener("click", getAllOwners);
+}
+
+const init = () => 
+{
     addEventListeners();
 }
 
 init();
-
