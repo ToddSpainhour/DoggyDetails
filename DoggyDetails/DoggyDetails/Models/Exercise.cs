2330f758df95611722dd5f93d05f75d01132bf8c
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-07-05 18:05:36 -0500

    other models/classes created
    created classes/models for Pet, Exercise, Food, Medicine, and Note

---
+++ DoggyDetails/DoggyDetails/Models/Exercise.cs
@@ -0,0 +1,11 @@
+﻿namespace DoggyDetails.Models
+{
+    public class Exercise
+    {
+        public int ExerciseID { get; set; }
+        public int? PetID { get; set; }
+        public string? Type { get; set; }
+        public float? Distance { get; set; }
+        public DateTime? TimeStamp { get; set; }
+    }
+}
---
+++ DoggyDetails/DoggyDetails/Models/Pet.cs
@@ -0,0 +1,13 @@
+﻿namespace DoggyDetails.Models
+{
+    public class Pet
+    {
+        public int PetID { get; set; }
+
+        public int OwnerID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
