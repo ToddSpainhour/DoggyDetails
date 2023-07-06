441a0e56fa86af096afa0923ab4a1bf4c69ded95
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-07-05 20:01:41 -0500

    created empty http functions for frontend
    - created empty http methods for pet, exercise, food, medicine, and note
    - imported all functions into dashboard.js

---
+++ DoggyDetails.ui/javascripts/helpers/petData.js
@@ -0,0 +1,22 @@
+
+const getPet = async (petID) =>
+{
+    // hit getPet endpoint
+}
+
+const createNewPet = async (petDetails) =>
+{
+    // hit createNewPet endpoint
+}
+
+const editPet = async (petDetails) =>
+{
+    // hit editPet endpoint
+}
+
+const deletePet = (petID) =>
+{
+    // hit deletePet endpoint
+}
+
+export {getPet, createNewPet, editPet, deletePet}
