7875997a6b8c78139f48746c2a49e1624ac47e31
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-07-07 19:04:48 -0500

    added try catch blocks
    - added try catch blocks that log errors

--- DoggyDetails.ui/javascripts/createAccount.js
+++ DoggyDetails.ui/javascripts/createAccount.js
@@ -8,12 +8,12 @@ const changeUsernameUnavailableVisability = (dbResponse) =>
     
     if (dbResponse == "False")
     {
-        usernameAlreadyTakenMessage.innerHTML = "That username is not available. Try again"
+        usernameAlreadyTakenMessage.innerHTML = "That Account Email is not available. Try again"
         createAccountInputContainer.style.display = "none";
     } 
     else 
     {
-        usernameAlreadyTakenMessage.innerHTML = "Great. Let's keep going!"
+        usernameAlreadyTakenMessage.innerHTML = "Great, that Account Email is available for you to use! Let's keep going!"
         createAccountInputContainer.style.display = "block";
     }
 }
@@ -22,37 +22,57 @@ const CheckUsernameAvailability = async () =>
 {
     const userEnteredValue = document.getElementById("username").value;
     const url = `${baseUrl}/Owners/UsernameAvailability/${userEnteredValue}`
+
+try
+{
     let response = await fetch(url);
     let responseAsText = await response.text();
     const isUsernameAvailabile = responseAsText;
     changeUsernameUnavailableVisability(isUsernameAvailabile);
+} 
+catch(err)
+{
+    console.log(`Oh, no! Something went wrong in the CheckUsernameAvailability function. Error Info: ${err}`)
+}
+
+
+
 }
 
 const createNewOwner = async () => {
 
-    const username = document.getElementById('username').value
-    const password = document.getElementById('passwordInput').value
-    const firstName = document.getElementById('firstName').value
-    const lastName = document.getElementById('lastName').value
-
-    const newOwner = {
-        firstName: firstName,
-        lastName: lastName,
-        accountEmail: username,
-        accountPassword: password
+    try 
+    {
+        const username = document.getElementById('username').value
+        const password = document.getElementById('passwordInput').value
+        const firstName = document.getElementById('firstName').value
+        const lastName = document.getElementById('lastName').value
+    
+        const newOwner = {
+            firstName: firstName,
+            lastName: lastName,
+            accountEmail: username,
+            accountPassword: password
+        }
+    
+        const response = await fetch(`${baseUrl}/Owners/createNewOwner/${newOwner}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(newOwner),
+        });
+    
+        const result = await response.text();
+        document.cookie = `OwnerID=${result};`;
+        pushToDashboard();
+    } 
+    catch (err) 
+    {
+        console.log(`Oh, no! Something went wrong in the CheckUsernameAvailability function. Error Info: ${err}`)
     }
 
-    const response = await fetch(`${baseUrl}/Owners/createNewOwner/${newOwner}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newOwner),
-    });
 
-    const result = await response.text();
-    document.cookie = `OwnerID=${result};`;
-    pushToDashboard();
 }
 
 const pushToDashboard = () => {
